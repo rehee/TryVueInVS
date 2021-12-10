73349ceaae47d55f699fe0539ca441255a0e3371
rehee <x3iii131@gmail.com>
2021-12-10 19:01:03 +0000

    hide js file generate

--- TryVueInVS/src/funcs/HttpService.ts
+++ TryVueInVS/src/funcs/HttpService.ts
@@ -1,13 +1,15 @@
-import { Axios } from 'axios';
-const axios: Axios = require('axios').default;
+import { Axios } from "axios";
+const axios: Axios = require("axios").default;
 
 export class HttpFunc {
-  public static get() {
+  public static get(): void {
     axios.get("https://reheene6.herokuapp.com/api/property/type/0")
       .then(r => {
-      console.log(r);
-    });
+        console.log(r);
+      });
+  }
+  public static gge2t(): void {
+
   }
 
-  
 }
